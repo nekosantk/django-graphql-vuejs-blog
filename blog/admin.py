4d7bb48db889876acc1896da016a4677edf8d0df
Mark Louw <24228731+nekosantk@users.noreply.github.com>
2023-02-27 12:33:36 +0200

    Added project files (#1)

---
+++ blog/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from blog.models import Profile, Post, Tag
+
+
+# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
+    model = Tag
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        "id",
+        "title",
+        "subtitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+
+    list_filter = (
+        "published",
+        "publish_date",
+    )
+
+    list_editable = (
+        "title",
+        "subtitle",
+        "slug",
+        "publish_date",
+        "published",
+    )
+
+    search_fields = (
+        "title",
+        "subtitle",
+        "slug",
+        "body",
+    )
+
+    prepopulated_fields = {
+        "slug": (
+            "title",
+            "subtitle",
+        )
+    }
+
+    date_heirarchy = "publish_date"
+    save_on_tope = True
